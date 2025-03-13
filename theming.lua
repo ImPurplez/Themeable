edfd9064770eee1a64bd2bfd109282949c72e3fa
ImPurplez_ <98240048+ImPurplez@users.noreply.github.com>
2025-03-13 12:51:52 -0400

    addedMain

---
+++ theming.lua
@@ -0,0 +1,8 @@
+local themeableConfigPath = "Mods/themeable/config.json"
+local themeable
+for i, v in ipairs(mods) do
+    if v.id == "themeable" then
+        themeable = i
+        break
+    end
+end
